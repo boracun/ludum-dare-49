41c08413ab3a3fdfc8166750a5d3de512cc0224c
boracun <cun2424@gmail.com>
2021-10-02 22:12:30 +0300

    Create Waiter GameObject

---
+++ Assets/Scripts/Movement/WaiterMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movement
+{
+    public class WaiterMovement : MonoBehaviour
+    {
+        public float playerSpeed;
+        public Rigidbody2D rigidbody2D;
+        public SpriteRenderer spriteRenderer;       //Move this to Animation Code when it is created
+        private Vector2 _playerDirection;
+
+        // Update is called once per frame
+        void Update()
+        {
+            float directionX;
+            if (Input.GetKey(KeyCode.A))
+            {
+                directionX = -1f;
+                spriteRenderer.flipX = true;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                directionX = 1f;
+                spriteRenderer.flipX = false;
+            }
+            else directionX = 0f;
+
+            float directionY;
+            if (Input.GetKey(KeyCode.S)) directionY = -1f;
+            else if (Input.GetKey(KeyCode.W)) directionY = 1f;
+            else directionY = 0f;
+
+            _playerDirection = new Vector2(directionX, directionY).normalized;
+        }
+
+        void FixedUpdate()
+        {
+            rigidbody2D.velocity = new Vector2(_playerDirection.x * playerSpeed, _playerDirection.y * playerSpeed);
+        }
+    }
+}
+
