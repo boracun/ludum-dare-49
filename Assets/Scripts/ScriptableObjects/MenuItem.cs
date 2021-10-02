cb0e6ecc9b33aed13b1efe84a7652e0aacc7e516
boracun <cun2424@gmail.com>
2021-10-03 02:56:05 +0300

    Add Clone method to MenuItem.cs

--- Assets/Scripts/ScriptableObjects/MenuItem.cs
+++ Assets/Scripts/ScriptableObjects/MenuItem.cs
@@ -1,9 +1,20 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Menu Item", menuName = "MenuItem")]
+[Serializable]
 public class MenuItem : ScriptableObject
 {
     public string menuItemName;
     
     public Sprite menuItemSprite;
+
+    public MenuItem Clone()
+    {
+        MenuItem clone = CreateInstance<MenuItem>();
+        clone.menuItemName = menuItemName;
+        clone.menuItemSprite = menuItemSprite;
+
+        return clone;
+    }
 }
