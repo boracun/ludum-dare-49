6ed720f6eb934f6381c5a8e1b6261a5dbc2777e0
Cagri Eren <cagri.eren@yahoo.co.uk>
2021-10-02 18:18:07 +0300

    Table object class implemented

---
+++ Assets/Scripts/Objects/Table.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace Objects
+{
+    public class Table : MonoBehaviour
+    {
+        public float maxCount;
+
+        public GameObject[] customers;
+    }
+}
