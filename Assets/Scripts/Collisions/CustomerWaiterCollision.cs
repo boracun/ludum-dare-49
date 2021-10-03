2393823400515c4e5298162503d595cda555bbea
boracun <cun2424@gmail.com>
2021-10-04 00:08:16 +0300

    Delete unnecessary comments

--- Assets/Scripts/Collisions/CustomerWaiterCollision.cs
+++ Assets/Scripts/Collisions/CustomerWaiterCollision.cs
@@ -7,18 +7,6 @@ namespace Collisions
 {
     public class CustomerWaiterCollision : MonoBehaviour
     {
-        /*private void OnCollisionEnter2D(Collision2D other)
-        {
-            if (other.collider.CompareTag("Customer"))
-            {
-                if (other.collider.gameObject.GetComponent<CustomerMovement>().movementMode !=
-                    CustomerMovement.WAIT_MODE)
-                {
-                    Waiter.Instance.DropItem();
-                }
-            }
-        }*/
-
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Customer"))
