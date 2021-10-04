b2578206afde7fe908d1c8b044c7e48100cffc1b
boracun <cun2424@gmail.com>
2021-10-04 21:17:03 +0300

    Add Reputation Bar to top right

--- Assets/Scripts/Manager/ReputationManager.cs
+++ Assets/Scripts/Manager/ReputationManager.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Manager
 {
@@ -7,13 +9,37 @@ namespace Manager
     {
         public float reputation = 100f;
         public float reputationLoseLimit = 30f;
+        public GameObject reputationBar;
+        private GameObject reputationBarProgress;
+        public Text reputationBarText;
+        private float startScaleX;
+
+        private void Start()
+        {
+            reputationBarProgress = reputationBar.transform.GetChild(2).gameObject;
+            startScaleX = reputationBarProgress.transform.localScale.x;
+        }
 
         private void Update()
         {
+            UpdateReputationBar();
             if (reputation < reputationLoseLimit)
             {
                 SceneManager.LoadScene("Ending");
             }
+        }
+
+        private void UpdateReputationBar()
+        {
+            reputationBarText.text = reputation + "%";
+            var localScale = reputationBarProgress.transform.localScale;
+            localScale = new Vector3(reputation * startScaleX / 100f,
+                localScale.y, localScale.z);
+            reputationBarProgress.transform.localScale = localScale;
+
+            var transformPosition = reputationBarProgress.transform.localPosition;
+            transformPosition.x = (startScaleX - localScale.x) / -2f;
+            reputationBarProgress.transform.localPosition = transformPosition;
         }
     }
 }
