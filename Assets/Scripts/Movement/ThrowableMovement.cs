5333144adfa334f30b9d3315af7976015237ecdf
boracun <cun2424@gmail.com>
2021-10-04 22:19:41 +0300

    Fix Throwable Movement

--- Assets/Scripts/Movement/ThrowableMovement.cs
+++ Assets/Scripts/Movement/ThrowableMovement.cs
@@ -9,7 +9,7 @@ public class ThrowableMovement : MonoBehaviour
 
     public Rigidbody2D rBody;
 
-    public float speed = 2f;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     { 
@@ -22,13 +22,14 @@ public class ThrowableMovement : MonoBehaviour
         var position = transform.position;
         float goalX = waiterX - position.x;
         float goalY = waiterY - position.y;
-        rBody.velocity = new Vector2((Math.Abs(goalX) / (float)(Math.Pow(Math.Abs(goalX), 2f) + Math.Pow(Math.Abs(goalY), 2f))) * speed,
-            (Math.Abs(goalY) / (float)(Math.Pow(Math.Abs(goalX), 2f) + Math.Pow(Math.Abs(goalY), 2f))) * speed);
+        /*Vector2 direction = new Vector2((Math.Abs(goalX) / (float)Math.Sqrt((Math.Pow(Math.Abs(goalX), 2f) + Math.Pow(Math.Abs(goalY), 2f)))),
+            (Math.Abs(goalY) / (float)Math.Sqrt(Math.Pow(Math.Abs(goalX), 2f) + Math.Pow(Math.Abs(goalY), 2f)))).normalized;*/
+        Vector2 direction = new Vector2(goalX, goalY).normalized;
+        rBody.velocity = new Vector2(direction.x * speed, direction.y * speed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Rotate(Vector3.forward, 4f);
     }
 }
